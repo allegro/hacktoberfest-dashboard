7b1c0f645a6dfe52688fd5c56d6302d2b52720c5
Bartosz Galek <bartosz@galek.com.pl>
2019-10-24 18:02:25 +0200

    Updates

---
+++ precache-manifest.ca78ae65f45a3535c2be24e51764278d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a89e2082632ce90d3026024b1a3f8cd",
+    "url": "/hacktoberfest-dashboard/index.html"
+  },
+  {
+    "revision": "3b02d3c2ef6702fa7174",
+    "url": "/hacktoberfest-dashboard/static/css/main.545390d6.chunk.css"
+  },
+  {
+    "revision": "9bf1ae7809a932204d42",
+    "url": "/hacktoberfest-dashboard/static/js/2.6f7af17c.chunk.js"
+  },
+  {
+    "revision": "3b02d3c2ef6702fa7174",
+    "url": "/hacktoberfest-dashboard/static/js/main.3ca861ba.chunk.js"
+  },
+  {
+    "revision": "df6b9563932bf8ec6264",
+    "url": "/hacktoberfest-dashboard/static/js/runtime-main.2dd5f773.js"
+  },
+  {
+    "revision": "b101d99d42490278e92eb4ab00babc93",
+    "url": "/hacktoberfest-dashboard/static/media/allegro.b101d99d.svg"
+  },
+  {
+    "revision": "cc105787c967f12e5de0a1bf0a7425c4",
+    "url": "/hacktoberfest-dashboard/static/media/logo.cc105787.svg"
+  }
+]);
