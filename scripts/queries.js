333216860a7807e3f1ad736d334678915fd299fa
Bartosz Gałek <bartosz@galek.com.pl>
2022-10-13 23:12:38 +0200

    Update queries.js

--- scripts/queries.js
+++ scripts/queries.js
@@ -6,9 +6,13 @@
  */
 
 export function getPullRequests(userNames, year) {
+    let eventStartDate = `${year}-10-01`;
+    if (year = 2022) {
+        eventStartDate = `${year}-10-14`;
+    }
     return `
       {
-        search(query: "is:pr created:${year}-10-01..${year}-10-31 ${userNames.map(username => `author:${username}`).join(' ')}", type: ISSUE, first: 100) {
+        search(query: "is:pr created:${eventStartDate}..${year}-10-31 ${userNames.map(username => `author:${username}`).join(' ')}", type: ISSUE, first: 100) {
           pageInfo {
             endCursor
             startCursor
