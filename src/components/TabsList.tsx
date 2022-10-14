8e68ddd00ac26350d93ec00cbd5dff78d4434cc5
Tomasz Tarczyński <tomasz.tarczynski@allegro.pl>
2022-10-14 13:11:09 +0200

    Bind query params to tabs

--- src/components/TabsList.tsx
+++ src/components/TabsList.tsx
@@ -1,18 +1,24 @@
 import { Tabs } from '@mantine/core';
 import React from 'react';
 
-export function TabsList({ tabs }: { tabs: string[] }) {
+export function TabsList({
+  tabs,
+  onClick,
+}: {
+  tabs: string[];
+  onClick: (event: React.MouseEvent<HTMLElement>) => void;
+}) {
   return (
     <Tabs.List>
       {tabs.map((tab) => (
-        <Tabs.Tab value={tab} key={tab}>
+        <Tabs.Tab value={tab} key={tab} onClick={onClick}>
           {tab}
         </Tabs.Tab>
       ))}
-      <Tabs.Tab value='participants' key='participants'>
+      <Tabs.Tab value='participants' key='participants' onClick={onClick}>
         Participants
       </Tabs.Tab>
-      <Tabs.Tab value='inspirations' key='inspirations'>
+      <Tabs.Tab value='inspirations' key='inspirations' onClick={onClick}>
         Inspirations
       </Tabs.Tab>
     </Tabs.List>
