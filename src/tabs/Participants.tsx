5cf44ce681283ab1058ef3b56ad48c624dccd062
Tomasz Tarczyński <tomasz.tarczynski@allegro.pl>
2022-10-14 15:22:59 +0200

    Add unique key to participant's list

--- src/tabs/Participants.tsx
+++ src/tabs/Participants.tsx
@@ -9,7 +9,7 @@ export default function Participants({ participants }: { participants: string[] 
                 <StatsCard label="Total participants" value={participants.length}/>
                 <Paper withBorder radius="md" p="xs">
                     <List spacing="xs" size="sm" center>
-                        {participants.map(login => <Participant name={login}/>)}
+                        {participants.map(login => <Participant key={login} name={login}/>)}
                     </List>
                 </Paper>
             </Stack>
