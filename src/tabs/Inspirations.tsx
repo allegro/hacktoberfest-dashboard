dc91089a6cfc61217570be6bc272c56ff2e0f3e0
Adrianna Malkiewicz-Blotniak <adrianna.blotniak@allegro.pl>
2022-10-14 15:08:08 +0200

    Add Hacktober badge to the repository list

--- src/tabs/Inspirations.tsx
+++ src/tabs/Inspirations.tsx
@@ -2,6 +2,30 @@ import { Anchor, Container, List, Text, Title } from '@mantine/core';
 import repositories from '../data/repositories.json';
 import React from 'react';
 
+const GITHUB_LINK = 'https://github.com/';
+
+const renderRepositories = () =>
+    repositories.map((it) => {
+        const year = new Date().getFullYear();
+        const githubName = it.substring(GITHUB_LINK.length);
+        const isProject = githubName.split('/').length > 1;
+
+        return (
+            <List.Item key={it}>
+                <div className="repository">
+                    <Anchor href={it}>{githubName}</Anchor>
+                    {
+                        isProject &&
+                        <img
+                            alt="GitHub Hacktoberfest combined status"
+                            src={`https://img.shields.io/github/hacktoberfest/${year}/${githubName}`}
+                        />
+                    }
+                </div>
+            </List.Item>
+        );
+    })
+
 export function Inspirations({
   inspirations,
 }: {
@@ -20,11 +44,7 @@ export function Inspirations({
       </List>
       <Title py='lg'>Repositories</Title>
       <List spacing='xs' size='sm' center withPadding>
-        {repositories.map((it) => (
-          <List.Item key={it}>
-            <Anchor href={it}>{it}</Anchor>
-          </List.Item>
-        ))}
+        {renderRepositories()}
       </List>
     </Container>
   );
