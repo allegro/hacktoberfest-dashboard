349f1f1e7a14a8ed8a6f37cd9328813c74309e4e
Bartosz Gałek <bartosz@galek.com.pl>
2024-10-16 17:45:39 +0200

    Revert "Update contributions.js to fetch data only from current year"

--- scripts/contributions.js
+++ scripts/contributions.js
@@ -17,46 +17,43 @@ const contributions = {
     updatedAt: new Date().toISOString(),
 };
 
-let year = now.getFullYear();
-const users = JSON.parse(fs.readFileSync(`./src/data/users/${year}.json`));
+for (let year = now.getFullYear() - 3; year <= now.getFullYear(); year++) {
+    const users = JSON.parse(fs.readFileSync(`./src/data/users/${year}.json`));
 
-const results = [];
-let cursor = null;
-let morePages = true;
+    const results = [];
+    let cursor = null;
+    let morePages = true;
 
-while (morePages) {
-    const page = await fetch('https://api.github.com/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-            authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({ query: getPullRequests(users, year, cursor) }),
-    })
-      .then((response) => response.json())
-      .then(response => {
-          if (response.data.search.pageInfo.hasNextPage) {
-              cursor = `"${response.data.search.pageInfo.endCursor}"`;
-          } else {
-              morePages = false;
-          }
-          return response;
-      })
-      .then((it) => it.data)
-      .then(format);
-    results.push(page);
+    while (morePages) {
+        const page = await fetch('https://api.github.com/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({ query: getPullRequests(users, year, cursor) }),
+        })
+            .then((response) => response.json())
+            .then(response => {
+                if (response.data.search.pageInfo.hasNextPage) {
+                    cursor = `"${response.data.search.pageInfo.endCursor}"`;
+                } else {
+                    morePages = false;
+                }
+                return response;
+            })
+            .then((it) => it.data)
+            .then(format);
+        results.push(page);
+    }
+    contributions.data[year] = Object.values(groupByKey(results.reduce((acc, o) => {
+        acc.push(o);
+        return acc;
+    }, []).flat(), 'login')).map(list => list.reduce((acc, o) => ({ ...o, contributions: [...o.contributions, ...acc.contributions], score: o.score + acc.score }), { contributions: [], score: 0 }))
+
+    console.log(`Gathered ${year} contributions.`);
 }
-contributions.data[year] = Object.values(groupByKey(results.reduce((acc, o) => {
-    acc.push(o);
-    return acc;
-}, []).flat(), 'login')).map(list => list.reduce((acc, o) => ({
-    ...o,
-    contributions: [...o.contributions, ...acc.contributions],
-    score: o.score + acc.score,
-}), { contributions: [], score: 0 }));
-
-console.log(`Gathered ${year} contributions.`);
 
 fs.writeFileSync(path.resolve(`./src/data/contributions.json`), JSON.stringify(contributions));
 
